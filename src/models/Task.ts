8dfce16925e3f76bb09645d91a405dd4debe9126
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-15 00:42:21 +0200

    feat: Add more validation to taskSchema

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 import { ITask } from "../types/interfaces";
 
 const taskSchema = new mongoose.Schema<ITask>({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: [true, "Must provide a name"],
+    trim: true,
+    maxlength: [20, "Name can not be more than 20 characters"],
+  },
   completed: { type: Boolean, required: true },
 });
 

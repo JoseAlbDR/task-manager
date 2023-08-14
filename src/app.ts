f55397fbbcc801ce3efb7fd37fbab713c5d09aaf
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-14 16:30:45 +0200

    feat: all tasks router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from "express";
-
+import tasksRouter from "./routes/tasks";
 const app = express();
+const port = 3000;
 
-app.get("/", (_req: Request, res: Response) => {
+// routes
+app.get("/hello", (_req: Request, res: Response) => {
   res.send("Hello there!");
 });
 
-app.listen(3000);
+app.use("/api/v1/tasks", tasksRouter);
+
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}...`);
+});

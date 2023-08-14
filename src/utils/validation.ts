07240b6149725c0fbd380855ab0c27bd789e0310
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-15 00:44:15 +0200

    feat: add custom msg and options to Joi validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -3,9 +3,11 @@ import { ITask } from "../types/interfaces";
 
 export const validateTaskData = (task: ITask) => {
   const taskSchema = Joi.object<ITask>({
-    name: Joi.string().required(),
+    name: Joi.string().max(20).required().messages({
+      "string.max": "Name can not be more than 20 characters",
+    }),
     completed: Joi.boolean(),
   });
 
-  return taskSchema.validate(task);
+  return taskSchema.validate(task, { errors: { wrap: { label: false } } });
 };

289ec3206913d5f62b238d2d50edbbac67c9bb3e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-15 21:30:22 +0200

    feat: change task type to unknown for body validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 import { ITask } from "../types/interfaces";
 
-export const validateTaskData = (task: ITask) => {
+export const validateTaskData = (task: unknown) => {
   const taskSchema = Joi.object<ITask>({
     name: Joi.string().max(20).required().label("Name"),
     completed: Joi.boolean().label("Completed"),
